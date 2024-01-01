60d7f3f91a3a8f7e438c745176e382f8421d4da3
markbtesh <63694092+markbtesh@users.noreply.github.com>
2024-01-01 15:12:17 -0500

    Update AnimatedImage.jsx

--- src/AnimatedImage.jsx
+++ src/AnimatedImage.jsx
@@ -1,54 +1,68 @@
-// AnimatedImages.js
-import { motion } from 'framer-motion';
+import { motion, useAnimation } from "framer-motion";
+import { useState, useEffect } from "react";
 
 const AnimatedImages = () => {
-  const images = [
-    '/r1.png',
-    '/r2.png',
-    '/r3.png',
-    '/r4.png',
-  ];
-   
-  const xVal = ['00%', '-100%', '50%', '-200%'];
-  const scaleVal = [0.0001, 0.1, 0.003, 0.001]
-  
-  return (
-      <div style={{ position: 'relative', width: '100%', maxWidth: '500px', margin: '25% auto', scale: '0.5' }}>
-        {images.map((src, index) => (
-          <motion.div
-            key={index}
-            style={{ position: 'absolute', top: 0, left: 0 }}
-            initial={{ opacity: 0, x: xVal[index], y: '-200%',  rotate: 540, scale: scaleVal[index] }}
-            animate={{ opacity: 1, x: 0, y: 0, rotate: 0, scale: 1 }}
-            transition={{ duration: 1, delay: index * 0.2 }}
-          >
-            <img
-              src={src}
-              alt={`Image ${index + 1}`}
-              width={350}
-              height={150}
-            />
-          </motion.div>
-        
-        ))}
+  const images = ["/rc1.png", "/rc2.png", "/rc3.png", "/rc4.png"];
+  const controls = useAnimation();
+  const [isHovering, setIsHovering] = useState(false);
 
-        <motion.div
-          style={{ position: 'absolute', top: 153, left: 0 }}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1, x: 0}}
-          transition={{ duration: 1, delay:  2 }}
-        >
-          <img
-            src="/magenta.png"
-            alt={`Image magenta`}
-            width={350}
-            height={70}
-          />
-        </motion.div>
-      </div>
-    );
+  const startAnimation = async () => {
+    await controls.start((i) => ({
+      rotate: 0,
+      transition: { duration: 1, delay: i * 0.2 },
+    }));
   };
 
+  const hoverAnimation = async () => {
+    await controls.start((i) => ({
+      rotate: 540,
+      transition: { duration: 1, delay: i * 0.2 },
+    }));
+    await controls.start((i) => ({
+      rotate: 0,
+      transition: { duration: 1, delay: i * 0.2 },
+    }));
+  };
 
+  // Start the initial animation when the component mounts
+  useEffect(() => {
+    startAnimation();
+  }, []); // Empty dependency array means this effect runs once on mount
+
+  return (
+    <motion.div
+      style={{
+        position: "relative",
+        width: "100%",
+        maxWidth: "500px",
+        margin: "25% auto",
+        scale: "0.7",
+      }}
+      onHoverStart={() => {
+        setIsHovering(true);
+        hoverAnimation();
+      }}
+      onHoverEnd={() => setIsHovering(false)}
+    >
+      {images.map((src, index) => (
+        <motion.div
+          key={index}
+          custom={index}
+          initial={{ rotate: 540 }}
+          animate={controls}
+          style={{ position: "absolute", top: 0, left: 0 }}
+          transition={{ duration: 1, delay: index * 0.2 }}
+        >
+          <img
+            src={src}
+            alt={`Image ${index + 1}`}
+            width={350}
+            height={350}
+          />
+        </motion.div>
+      ))}
+    </motion.div>
+  );
+};
 
 export default AnimatedImages;
